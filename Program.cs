df4cd07613696147a5544c2a465677562b1106dd
Viler2210 <61444425+Viler2210@users.noreply.github.com>
2020-02-26 09:02:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UP0403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
